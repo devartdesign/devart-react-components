bef38cdfe5cef7baed1a2bf00330e9bd5749b507
Ramiro Gonzalez <rgonzalez@makingsense.com>
2019-09-15 13:28:25 -0300

    Remove key prop from Chip

--- dist/components/Chip/Chip.d.ts
+++ dist/components/Chip/Chip.d.ts
@@ -2,9 +2,8 @@ import React from 'react';
 export interface IChipProps {
     label: string;
     icon: any;
-    key?: string | number;
     className?: string;
     onClick: () => void;
 }
-declare const _default: React.MemoExoticComponent<({ onClick, label, key, icon, className }: IChipProps) => JSX.Element>;
+declare const _default: React.MemoExoticComponent<({ onClick, label, icon, className }: IChipProps) => JSX.Element>;
 export default _default;
--- src/components/Chip/Chip.tsx
+++ src/components/Chip/Chip.tsx
@@ -6,15 +6,14 @@ import styles from './styles';
 export interface IChipProps {
   label: string;
   icon: any;
-  key?: string | number;
   className?: string;
   onClick: () => void;
 }
 
-const Chip = ({ onClick, label, key, icon, className }: IChipProps) => {
+const Chip = ({ onClick, label, icon, className }: IChipProps) => {
   const Icon = icon;
   return (
-    <div role="button" key={key} className={`${css(styles.container)} ${className}`} onClick={onClick}>
+    <div role="button" className={`${css(styles.container)} ${className}`} onClick={onClick}>
       <span className={css(styles.label)}>{label}</span>
       {icon && <Icon />}
     </div>
